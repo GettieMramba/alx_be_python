3b9948f2365ac0c6dc9e68adb15dc0f797f7ae30
GettieMramba <162892951+GettieMramba@users.noreply.github.com>
2025-10-05 15:53:54 +0300

    adding folder and its files

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter first number, "))
+num2 = int(input("Enter second number, "))
+
+operation = input("Choose the operation, +, -,*, /,  ")
+
+if operation == "+":
+    print(num1 + num2)
+
+elif operation == "-":
+    print(num1 - num2)
+
+elif operation == "*":
+    print(num1 - num2)
+
+elif operation == "/":
+    print(num1 / num2)
+
+else:
+    print("invalid operation choice.")
