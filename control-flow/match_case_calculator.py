7d9c9c53a0ad729e93658be83e4a35fc819baa1b
GettieMramba <162892951+GettieMramba@users.noreply.github.com>
2025-10-05 16:49:09 +0300

    removed the commas in the script

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,26 +1,26 @@
-num1 = int(input("Enter the first number,"))
-num2 = int(input("Enter the second number,"))
-operation = input("Choose the operation, +, -,*, /,  ")
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+operation = input("Choose the operation: +, -,*, /,:")
 
 match operation:
 	case "+":
 		result = num1 + num2
-		print("The result  is", result)
+		print("The result  is" result)
 
 	case "-":
 		result = num1 - num2
-		print("The result  is", result)
+		print("The result  is" result)
 
 	case "*":
 		result = num1 * num2
-		print("The result  is", result)
+		print("The result  is" result)
 
 	case "/":
 		if num2 == 0:
 			print("Cannot divide by zero.")
 		else:
 			result = num1 / num2
-			print("The result is", result)
+			print("The result is" result)
 
 	case _:
 		print("Invalid operation choice.")
