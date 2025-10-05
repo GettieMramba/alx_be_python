311530e03e2649646ea8581866ef086164e9f0f6
GettieMramba <162892951+GettieMramba@users.noreply.github.com>
2025-10-05 16:39:17 +0300

    fixed the indentation in the script

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,24 +3,24 @@ num2 = int(input("Enter second number, "))
 operation = input("Choose the operation, +, -,*, /,  ")
 
 match operation:
-    case "+":
-        result = num1 + num2
-        print("The result  is", result)
+	case "+":
+		result = num1 + num2
+		print("The result  is", result)
 
-    case "-":
-        result = num1 - num2
-        print("The result  is", result)
+	case "-":
+		result = num1 - num2
+		print("The result  is", result)
 
-    case "*":
-        result = num1 * num2
-        print("The result  is", result)
+	case "*":
+		result = num1 * num2
+		print("The result  is", result)
 
-    case "/":
-    if num2 == 0:
-        print("Cannot divide by zero.")
-    else:
-        result = num1 / num2
-        print("The result is", result)
+	case "/":
+		if num2 == 0:
+			print("Cannot divide by zero.")
+		else:
+			result = num1 / num2
+			print("The result is", result)
 
-case _:
-    print("Invalid operation choice.")
+	case _:
+		print("Invalid operation choice.")
